723361a6b15621a53c1b96a0c3df4ac5e63f76df
Zei <raphael.zei@gmail.com>
2013-05-28 16:46:22 -0300

    Fixed of relationship between user and task/item

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,40 +1,40 @@
 class CustomersController < InheritedResources::Base
   respond_to :html, :xml, :json
-  actions :all
-  #def index
-  #  @customers = Customer.all
-  #  respond_with(@customers)
-  #end
-  #
-  #def show
-  #  @customer = Customer.find(params[:id])
-  #  respond_with(@customer)
-  #end
-  #
-  #def new
-  #  @customer = Customer.new
-  #  respond_with(@customer)
-  #end
-  #
-  #def edit
-  #  @customer = Customer.find(params[:id])
-  #end
-  #
-  #def create
-  #  @customer = Customer.new(params[:customer])
-  #  @customer.save
-  #  respond_with(@customer)
-  #end
-  #
-  #def update
-  #  @customer = Customer.find(params[:id])
-  #  @customer.update_attributes(params[:customer])
-  #  respond_with(@customer)
-  #end
-  #
-  #def destroy
-  #  @customer = Customer.find(params[:id])
-  #  @customer.destroy
-  #  respond_with(@customer)
-  #end
+
+  def index
+    @customers = Customer.all
+    respond_with(@customers)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+    respond_with(@customer)
+  end
+
+  def new
+    @customer = Customer.new
+    respond_with(@customer)
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def create
+    @customer = Customer.new(params[:customer])
+    @customer.save
+    respond_with(@customer)
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update_attributes(params[:customer])
+    respond_with(@customer, location: customers_path)
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    respond_with(@customer, location: customers_path)
+  end
 end
