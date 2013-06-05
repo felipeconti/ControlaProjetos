8c167dab776f9518550803c39ec91e1fc7bdbf16
Felipe Conti <felipeconti18@gmail.com>
2013-06-05 17:22:41 +0000

    Added itens of Type and State for db:seed; Fixed filter of _list on itens

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,9 +9,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #User.delete_all
-#State.delete_all
-#Type.delete_all
+State.delete_all
+Type.delete_all
 
-User.create!(email: 'USER_NAME', password: 'USER_PASS')
+#User.create!(email: 'contato@ztc.com.br', password: '1234')
+State.create!(id: 1, description: "Pendente")
+State.create!(id: 2, description: "Concluído")
+State.create!(id: 3, description: "Cancelado")
 
-#State.create!(email: 'USER_NAME', password: 'USER_PASS')
+Type.create!(id: 1, description: "Gestão de Projetos")
+Type.create!(id: 2, description: "Desenvolvimento")
+Type.create!(id: 3, description: "Bugs")
+Type.create!(id: 4, description: "Implantação")
+Type.create!(id: 5, description: "Relatórios")
+Type.create!(id: 6, description: "Documentação")
