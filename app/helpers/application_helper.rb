074ea60eee5455b211de7aa9e2acc507b7a6aaaf
Felipe Conti <felipeconti18@gmail.com>
2013-06-07 17:10:25 +0000

    Added count of itens in show of task and fixed navigations history

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,19 @@ module ApplicationHelper
     return toCut
   end
 
+  def get_where(type)
+      case type
+        when 1
+          "priority = 0 and state_id in (select id from states where UPPER(description) like 'PENDENTE' )"
+        when 2
+          "priority > 0 and state_id in (select id from states where UPPER(description) like 'PENDENTE' )"
+        when 3
+          "state_id = 2  OR state_id in (select id from states where UPPER(description) like 'CONCLU%DO' )"
+        when 4
+          "state_id = 3  OR state_id in (select id from states where UPPER(description) like 'CANCELADO' )"
+        else
+          ""
+        end
+    end
+
 end
