ecf8dffb1a176c45fdaf9c3856fd02dcd24fb0da
felipeconti <felipeconti18@gmail.com>
2013-06-07 19:41:15 -0300

    Fixed .js of bootstrap-datepicker-rails

--- app/assets/javascripts/bootstrap-datepicker.pt-BR.js
+++ app/assets/javascripts/bootstrap-datepicker.pt-BR.js
@@ -1,13 +1,11 @@
 /* Brazilian initialisation for the jQuery UI date picker plugin. */
 /* Written by Leonildo Costa Silva (leocsilva@gmail.com). */
 
-jQuery(function($){
-  $.datepicker.dates['pt-BR'] = {
-      days: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
-      daysShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
-      daysMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
-      months: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-      monthsShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
-      today: "Hoje"
-  };
+$.fn.datepicker.dates['pt-BR'] = {
+  days: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
+  daysShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+  daysMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+  months: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+  monthsShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+  today: "Hoje"
 };
