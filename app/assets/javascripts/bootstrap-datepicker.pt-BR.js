96f8a0956f11e1a82cb986763ae5eb0ebb86d6a6
Felipe Conti <felipeconti18@gmail.com>
2013-06-13 13:59:02 +0000

    Fixed tranlate of month Março in datepicker

--- app/assets/javascripts/bootstrap-datepicker.pt-BR.js
+++ app/assets/javascripts/bootstrap-datepicker.pt-BR.js
@@ -5,7 +5,7 @@ $.fn.datepicker.dates['pt-BR'] = {
   days: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
   daysShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
   daysMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
-  months: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+  months: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
   monthsShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
   today: "Hoje"
 };
