4917e6e8af1d345f1f756d773fc637ba5c3d6c73
felipeconti <felipeconti18@gmail.com>
2013-06-17 00:44:12 -0300

    Added column responsible of itens of task in list of tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,17 @@
 module TasksHelper
+  def getFirstItem(items)
+
+    @item = items.where('state_id = 1').first
+
+    if @item.nil?
+      ' '
+    else
+      if @item.user.nil?
+        ' '
+      else
+        @item.user.name
+      end
+    end
+
+  end
 end
