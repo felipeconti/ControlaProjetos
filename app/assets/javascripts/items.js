4a772286a43498cdb3b9883ee98ea5a3cce9ecba
raphaelzei <raphael.zei@gmail.com>
2013-06-18 15:28:59 -0300

    Fixed layout of form search and changed js

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,11 +1,11 @@
 $(function(){
     $('#btnFilterItem').click(function (e) {
         e.preventDefault();
-        stateId = $('#stateId').val();
+        var stateId = $('#stateId').val();
         if ( stateId != '' ) {
             location.href = '/user_items?stateId='+stateId;
         }else{
-            location.href = '/user_items';
+            alert('selecione uma situação');
         }
     });
 });
--- app/assets/javascripts/meeting_items.js
+++ app/assets/javascripts/meeting_items.js
@@ -1,11 +1,11 @@
 $(function(){
     $('#btnFilterMeetingItem').click(function (e) {
         e.preventDefault();
-        stateId = $('#stateId').val();
+        var stateId = $('#stateId').val();
         if ( stateId != '' ) {
             location.href = '/user_meeting_items?stateId='+stateId;
         }else{
-            location.href = '/user_meeting_items';
+            alert('selecione uma situação');
         }
     });
 });
