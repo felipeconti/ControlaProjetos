bb972d648222609dd6425341a686c07f7b23a318
Felipe Conti <felipeconti18@gmail.com>
2013-07-05 14:48:43 +0000

    Fixed search in ajax

--- app/assets/javascripts/meeting_items.js
+++ app/assets/javascripts/meeting_items.js
@@ -2,10 +2,10 @@ $(function(){
     $('#btnFilterMeetingItem').click(function (e) {
         e.preventDefault();
         var stateId = $('#stateId').val();
-        if ( stateId != '' ) {
-            $(".page-grid").load('/user_meeting_items_result?stateId='+stateId);
-        }else{
-            alert('selecione uma situação');
+        if ( stateId == '' ) {
+          $("#stateId").prop("selectedIndex", 1);
+          stateId = $('#stateId').val();
         }
+        $(".page-grid").load('/user_meeting_items_result?stateId='+stateId);
     });
 });
